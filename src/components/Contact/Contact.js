c381343a1e9e31ede0a9f0b31d2cee75b744778e
Hans Brough <hans.brough@yahoo.com>
2022-11-22 19:10:44 -0800

    initial changes, nav naming, contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -27,31 +27,22 @@ const Contact = (props) => {
 
   return (
     <div className={styles.root}>
-      <div className={styles.section}>
-        <h4>Send Us A Message</h4>
-        <p>
-          Our Customer Service team are here for all enquiries Monday to Friday,
-          9am - 5pm AEDT (Australian Eastern Daylight Savings Time).
-        </p>
-        <p>We look forward to hearing from you.</p>
-      </div>
 
       <div className={styles.section}>
         <h4>Phone</h4>
-        <p>+1 424 280 4971</p>
-        <p>Monday to Friday - 9am - 5pm AEDT</p>
+        <p>650-218-4172</p>
       </div>
 
       <div className={styles.section}>
         <h4>Email</h4>
         <p>
-          You can email our Customer Service team at customerservice@example.com
+          You can email us at info@broughdesign.com
           or via the contact form below:
         </p>
       </div>
 
       <div className={styles.contactContainer}>
-        <form onSubmit={(e) => handleSubmit(e)}>
+        <form onSubmit={(e) => handleSubmit(e)} name="contact" netlify>
           <div className={styles.contactForm}>
             <FormInputField
               id={'name'}
